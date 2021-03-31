a77eda92c2b466ba27e1393e9fca3960b3df21f2
Meriam Ourri <ourrimeriam@gmail.com>
2021-03-31 16:54:00 -0400

    Adding building animation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var box1 = document.querySelector('.box1');
 var box2 = document.querySelector('.box2');
 var box3 = document.querySelector('.box3');
+var building = document.querySelector('.building');
 
 var domNode1 = anime({
     targets: box1,
@@ -27,4 +28,13 @@ var domNode1 = anime({
     easing: 'easeOutBounce',
     delay:20000,
     duration:1000
+});
+
+var domNode4 = anime({
+    targets: building,
+    translateY: 555,
+    translateX: 0,
+    easing: 'easeOutBounce',
+    delay:22000,
+    duration:2000
 });
