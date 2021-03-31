d1ee6ae578756780f5e63f52ddae9beae7e55c6d
Meriam Ourri <ourrimeriam@gmail.com>
2021-03-31 16:33:30 -0400

    Boxes animation using Anime.js

---
+++ app.js
@@ -0,0 +1,30 @@
+var box1 = document.querySelector('.box1');
+var box2 = document.querySelector('.box2');
+var box3 = document.querySelector('.box3');
+
+var domNode1 = anime({
+    targets: box1,
+    translateY: 600,
+    translateX: 0,
+    easing: 'easeOutBounce',
+    delay:18000,
+    duration:1000
+  });
+
+  var domNode2 = anime({
+    targets: box2,
+    translateY: 600,
+    translateX: 0,
+    easing: 'easeOutBounce',
+    delay:19000,
+    duration:1000
+});
+
+  var domNode3 = anime({
+    targets: box3,
+    translateY: 555,
+    translateX: 0,
+    easing: 'easeOutBounce',
+    delay:20000,
+    duration:1000
+});
